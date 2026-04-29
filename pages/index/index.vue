ce5dbe67cc8829738abae541505caa57fc39cfef
yangliu <634691481@qq.com>
2026-04-29 09:07:31 +0800

    feat(首页): 添加动态高度计算测试代码,禁用下拉刷新并优化分页配置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,23 @@
 <template>
   <yy-paging v-model="state.dataList" @query="queryList" ref="paging" @scroll="scroll" v-bind="pagingConfig">
-    <view class="flex-col gap-3 p-3"></view>
+    <!-- <view class="flex-col gap-3 p-3"></view> -->
+    <view class="w-8 bg-red-500" :style="{ height: topHeight + 'px' }">r555</view>
   </yy-paging>
 </template>
 
 <script setup>
   const pagingConfig = ref({
     auto: false,
-    refresherEnabled: true,
-    showRefresherWhenReload: true,
+    refresherEnabled: false,
+    showRefresherWhenReload: false,
     showTabbar: true,
     hideNav: false,
     showNavBack: true,
     navTitle: '首页',
     // navBackground: '',
     color: uni.$u.color.primary,
+    loadingMoreNoMoreText: '',
+    emptyText: '',
   })
 
   const state = ref({
@@ -22,10 +25,16 @@
     dataList: [],
   })
 
+  const topHeight = ref(0)
+
   const paging = ref()
 
   onLoad(options => {
     console.log('🚀 页面加载:', options)
+    const sysInfo = uni.getSystemInfoSync()
+    console.log('🚀 ~ :35 ~ sysInfo:', sysInfo)
+    const navHeight = (sysInfo.statusBarHeight || 0) + 55
+    topHeight.value = sysInfo.windowHeight - navHeight
   })
 
   onShow(options => {
